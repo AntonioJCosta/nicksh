774c77f23a31ae8d33c61edab1fae93b85341d47
AntonioJCosta <antoniojuncosta@gmail.com>
2025-05-10 19:08:54 -0300

    fix: update build script to use internal version for ldflags and improve output messages

--- .github/scripts/build_app.sh
+++ .github/scripts/build_app.sh
@@ -3,18 +3,17 @@ set -e
 
 echo "Building version: $APP_VERSION for $GOOS/$GOARCH"
 
-# Remove version prefix from APP_VERSION
-APP_VERSION="${APP_VERSION#v}"
+# Version for ldflags (without 'v' prefix)
+LD_APP_VERSION="${APP_VERSION#v}"
 
 cd cmd/nicksh
 
 BINARY_NAME_BASE="nicksh-${GOOS}-${GOARCH}"
-BINARY_NAME_WITH_VERSION="nicksh-${GOOS}-${GOARCH}-${APP_VERSION}"
-OUTPUT_BINARY_NAME="${BINARY_NAME_BASE}"
 ARCHIVE_NAME="${BINARY_NAME_BASE}-${APP_VERSION}.tar.gz" 
+OUTPUT_BINARY_NAME="${BINARY_NAME_BASE}"
 
-go build -v -trimpath -ldflags="-s -w -X main.Version=$APP_VERSION" -o "${OUTPUT_BINARY_NAME}" .
-echo "Built: ${OUTPUT_BINARY_NAME}"
+go build -v -trimpath -ldflags="-s -w -X main.Version=$LD_APP_VERSION" -o "${OUTPUT_BINARY_NAME}" .
+echo "Built: ${OUTPUT_BINARY_NAME} (internal version $LD_APP_VERSION)"
 
 tar -czvf "${ARCHIVE_NAME}" "${OUTPUT_BINARY_NAME}"
 echo "Archived: ${ARCHIVE_NAME}"
